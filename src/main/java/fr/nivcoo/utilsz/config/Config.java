e952311f75021ed6fc45bba8e47f597928807616
nivcoo <nico64800@live.fr>
2022-01-31 23:03:46 +0100

    improv. fix hex support

--- src/main/java/fr/nivcoo/utilsz/config/Config.java
+++ src/main/java/fr/nivcoo/utilsz/config/Config.java
@@ -26,7 +26,7 @@ public class Config {
         Matcher matcher = pattern.matcher(msg);
         while (matcher.find()) {
             String color = msg.substring(matcher.start(), matcher.end());
-            msg = msg.replace(color, ChatColor.of(color) + "");
+            msg = msg.replace(color, ChatColor.of(color.replace("&", "")) + "");
             matcher = pattern.matcher(msg);
 
         }
