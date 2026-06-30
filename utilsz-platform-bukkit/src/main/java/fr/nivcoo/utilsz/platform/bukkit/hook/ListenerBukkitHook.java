3171f02018b49416a6631784ab2e8a44739dfd40
nivcoo <nico64800@live.fr>
2026-06-30 10:18:13 +0200

    improv. update parent version to 1.3.91 in pom.xml and add onLoad method in ListenerBukkitHook and RepeatingBukkitHook for improved hook initialization

--- utilsz-platform-bukkit/src/main/java/fr/nivcoo/utilsz/platform/bukkit/hook/ListenerBukkitHook.java
+++ utilsz-platform-bukkit/src/main/java/fr/nivcoo/utilsz/platform/bukkit/hook/ListenerBukkitHook.java
@@ -3,8 +3,12 @@ package fr.nivcoo.utilsz.platform.bukkit.hook;
 import org.bukkit.event.Listener;
 
 public abstract class ListenerBukkitHook<C extends BukkitHookContext> implements BukkitHook<C>, Listener {
+    protected void onLoad(C context) {
+    }
+
     @Override
     public final void load(C context) {
+        onLoad(context);
         context.registerListener(this);
     }
 }
--- utilsz-platform-bukkit/src/main/java/fr/nivcoo/utilsz/platform/bukkit/hook/RepeatingBukkitHook.java
+++ utilsz-platform-bukkit/src/main/java/fr/nivcoo/utilsz/platform/bukkit/hook/RepeatingBukkitHook.java
@@ -1,10 +1,14 @@
 package fr.nivcoo.utilsz.platform.bukkit.hook;
 
 public abstract class RepeatingBukkitHook<C extends BukkitHookContext> implements BukkitHook<C>, Runnable {
+    protected void onLoad(C context) {
+    }
+
     protected abstract long periodTicks(C context);
 
     @Override
     public final void load(C context) {
+        onLoad(context);
         long period = periodTicks(context);
         context.runRepeating(this, period, period);
     }
